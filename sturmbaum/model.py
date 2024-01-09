4aa7a82b6933468bd18d4b4fd556354697a69d76
Jared Sulzdorf <j.sulzdorf@gmail.com>
2024-01-09 15:50:03 -0800

    API calls working

---
+++ sturmbaum/model.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class SensorData(Base):
+    __tablename__ = "sensor_data"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    published: Mapped[int]
+    humidity: Mapped[float]
+    airtemp: Mapped[float]
+    pressure: Mapped[float]
+    windspeed: Mapped[float]
+    winddirection: Mapped[float]
+    soiltemp: Mapped[float]
+    soilmoisture: Mapped[float]
+    rainfall: Mapped[float]
